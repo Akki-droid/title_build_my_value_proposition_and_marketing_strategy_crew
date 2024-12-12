e9370b9128670585378acc155305a79e32e6f199
Satya Prakash Pati <satyaprakashpati@Smitas-MacBook-Air.local>
2024-12-12 11:52:05 +0530

    Updateprod

--- src/title_build_my_value_proposition_and_marketing_strategy_crew/crew.py
+++ src/title_build_my_value_proposition_and_marketing_strategy_crew/crew.py
@@ -1,9 +1,9 @@
 from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
-from time import sleep
-#
-from threading import Event, Thread
 import os
+
+os.environ["anthropic_model"] = "anthropic/claude-3-haiku-20240307"
+
 
 @CrewBase
 class TitleBuildMyValuePropositionAndMarketingStrategyCrewCrew():
@@ -13,44 +13,32 @@ class TitleBuildMyValuePropositionAndMarketingStrategyCrewCrew():
     def value_proposition_canvas_creator(self) -> Agent:
         return Agent(
             config=self.agents_config['value_proposition_canvas_creator'],
-            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model="anthropic/claude-3-haiku-20240307", )
+            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model=os.getenv("anthropic_model"), )
         )
 
     @agent
     def marketing_analyst(self) -> Agent:
         return Agent(
             config=self.agents_config['marketing_analyst'],
-            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model="anthropic/claude-3-haiku-20240307",  )
+            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model=os.getenv("anthropic_model"),  )
         )
 
     @agent
     def swot_analyzer(self) -> Agent:
         return Agent(
             config=self.agents_config['swot_analyzer'],
-            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model="anthropic/claude-3-haiku-20240307", )
+            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"),model=os.getenv("anthropic_model"), )
         )
 
     @agent
     def buyers_journey_mapper(self) -> Agent:
         return Agent(
             config=self.agents_config['buyers_journey_mapper'],
-            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model="anthropic/claude-3-haiku-20240307", )
+            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model=os.getenv("anthropic_model"), )
         )
-
-    @agent
-    def output_formatter(self) -> Agent:
-        return Agent(
-            config=self.agents_config['output_formatter'],
-            llm=LLM(api_key=os.getenv("ANTHROPIC_API_KEY"), model="anthropic/claude-3-haiku-20240307",  )
-        )
-
 
     @task
     def create_value_proposition_canvas_task(self) -> Task:
-        print(f"Akki is analyzing the inputs...")
-        sleep(3),  # Simulate task duration
-        print(f"Task value_proposition_canvas_creator is in progress...!")
-        sleep(2),
         return Task(
             config=self.tasks_config['create_value_proposition_canvas_task'],
             output_file='output/report_canvas.md'
@@ -77,12 +65,6 @@ class TitleBuildMyValuePropositionAndMarketingStrategyCrewCrew():
             output_file='output/report_buyers.md'
         )
 
-    @task
-    def combine_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['combine_task'],
-            output_file='output/reports_combine.md'
-        )
 
     @crew
     def crew(self) -> Crew:
